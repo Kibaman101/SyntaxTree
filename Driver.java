55248b70a6fdc10b81232f4cb9707dd260c84625
Nick Schumacher <nicholas.schumacher@cuw.edu>
2015-04-30 09:56:41 -0500

    HW for April 30

--- Driver.java
+++ Driver.java
@@ -1,13 +1,14 @@
-
 public class Driver 
 {
-
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		Parser p = new Parser ("     a =       (a  *  b ");
-		Parser p2 = new Parser("     a = b -c;");
-
+		/*
+		Parser p = new Parser("     a =     (a *   b) - c;");
+		Parser p2 = new Parser("a = ((a+k) * (a + (b/n))) - c;");
+		//p.parse();
+		p2.parse();
+		*/
+		Parser p = new Parser("     apple =     (a *   b) - c;");
 		p.parse();
 	}
-
 }
--- Parser.java
+++ Parser.java
@@ -1,12 +1,14 @@
-
 public class Parser 
 {
 	private String theStmt;
-	private int pos;  //where am I in the theStmt string
-
+	private int pos; //where am I in the theStmt string
+	private static final String legalVariableCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "; 
+	private static final String legalOpCharacters = "+-*/% ";
+	
 	public Parser(String theStmt)
 	{
 		this.theStmt = theStmt;
+		
 		this.pos = 0;
 	}
 	
@@ -15,55 +17,89 @@ public class Parser
 		this.parse_stmt();
 	}
 	
+	private String getNextToken(char c)
+	{
+		while(pos < this.theStmt.length())
+		{
+			if(this.theStmt.charAt(pos) == c)
+			{
+				pos++;
+				break;
+			}
+			pos++;
+		}
+		return "" + c;
+	}
+	
+	private String getNextToken(String legalChars)
+	{
+		String token = "";
+		while(pos < this.theStmt.length())
+		{
+			if(legalChars.indexOf(this.theStmt.charAt(pos)) != -1)
+			{
+				token += this.theStmt.charAt(pos);
+			}
+			else
+			{
+				//this means we are at the end of the token
+				//We are always trimming leading and trailing spaces
+				//move forward one
+				break;
+			}
+			pos++;
+		}
+		return token.trim();
+	}
+	
 	private void parse_stmt()
 	{
-		for(i = 0; i < this.theStmt.length(); i++)
-		{
-			if(this.theStmt.charAt(i) == "a" || "b")
-			{
-				System.out.println("Left variable: " + this.theStmt.charAt(i));
-			}
-			else if(this.theStmt.charAt(i) == "a" || "b" || "c")
-			{
-				System.out.println("Right variable:" + this.theStmt.charAt(i));
-			}
-		}
-	
 		//Print each time it reads something like:
 		// Read: VarName = a
-		// Reading: Math-Expr
+		String varName = this.getNextToken(Parser.legalVariableCharacters);
+		System.out.println("Read VarName: " + varName);
 		
-		//read a var name
-		//read a math_expr
+		//burn past the =
+		this.getNextToken('=');
+		System.out.println("Burned =");
+		
+		// Reading: Math-Expr
+		this.parse_math_expr();
+		
+		//burn past the ;
+		this.getNextToken(';');
+		System.out.println("Burned ;");
 	}
 	
 	private void parse_math_expr()
 	{
-		for(i = 0; i < this.theStmt.length(); i++)
+		String varName = this.getNextToken(Parser.legalVariableCharacters);
+		if(varName.length() == 0)
 		{
-			if(this.theStmt.charAt(i) == "+" )
-			{
-				System.out.println("Math Expression: +");
-			}
-			else if(this.theStmt.charAt(i) == "-")
-			{
-				System.out.println("Math Expression: -");
-			}
-			else if(this.theStmt.charAt(i) == "*")
-			{
-				System.out.println("Math Expression: *");
-			}
-			else if(this.theStmt.charAt(i) == "/")
-			{
-				System.out.println("Math Expression: /");
-			}
-		//Display
-		//Reading Left:
-		//Read OP = *
-		//Reading Right
-		
-		//read left
-		//read op
-		//read right
+			//we know that we are at the beginning of a paren-math-expr
+			this.getNextToken('(');
+			System.out.println("Burned (");
+			this.parse_math_expr();
+			this.getNextToken(')');
+			System.out.println("Burned )");
+		}
+		else
+		{
+			System.out.println("Read VarName: " + varName);
+		}
+		String op = this.getNextToken(Parser.legalOpCharacters);
+		System.out.println("Read Op: " + op);
+		varName = this.getNextToken(Parser.legalVariableCharacters);
+		if(varName.length() == 0)
+		{
+			//we know that we are at the beginning of a paren-math-expr
+			this.getNextToken('(');
+			this.parse_math_expr();
+			this.getNextToken(')');
+		}
+		else
+		{
+			System.out.println("Read VarName: " + varName);
+		}
 	}
 }
